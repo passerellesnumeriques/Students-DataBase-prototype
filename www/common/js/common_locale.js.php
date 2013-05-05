eea5ea2e3fce544c055efbab966d4f05ad6752e6
Guillaume LE COUSIN <guillaume@lecousin.net>
2013-05-05 18:57:42 +0800

    - data list:   + add sorting functionalities   + add export to excel, pdf and csv - user management:   + assign and unassign roles functionalities   + view and edit role rights functionalities - ui:   + horizontal menu, to adapt to window size   + animations - locale: support accent for capital letters

--- www/common/js/common_locale.js.php
+++ www/common/js/common_locale.js.php
@@ -1,6 +1,9 @@
+if (typeof pn != 'undefined')
 pn.locale = {
 	Error: "<?php locale("Error")?>",
 	Confirmation: "<?php locale("Confirmation")?>",
 	Yes: "<?php locale("Yes")?>",
-	No: "<?php locale("No")?>"
+	No: "<?php locale("No")?>",
+	Ok: "<?php locale("Ok")?>",
+	Cancel: "<?php locale("Cancel")?>"
 };
--- www/component/user_management/page/users.php
+++ www/component/user_management/page/users.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once("component/data_model/list/DataList.inc");
 $list = new DataList("Users");
 $list->primary_key("Users.domain","Users.username");
@@ -7,9 +7,82 @@ $list->add("Users.username", false);
 $list->add("UserPeople.people>first_name", false);
 $list->add("UserPeople.people>last_name", false);
 $list->add("UserRole.role_id>name", false);
-$list->selectable();
-$list->add_header("<button onclick='pn.popup_page(\"".get_locale("Add user")."\",\"/static/common/images/add.png\",\"user_management\",\"add_user\");return false'><img src='/static/common/images/add.png' style='vertical-align:bottom'/>".get_locale("Add user")."</button>");
+if (PNApplication::$instance->user_management->has_right("assign_role")) {
+	$list->selectable();
+	$list->add_selection_action("<div class='button' onclick='um_users_assign_roles()'>".get_locale("Assign roles")."</div>");
+	$list->add_selection_action("<div class='button' onclick='um_users_unassign_roles()'>".get_locale("Unassign roles")."</div>");
+}
+$list->add_header("<div class='button' onclick='alert(\"TODO\");'><img src='/static/common/images/add.png' style='vertical-align:bottom'/>".get_locale("Add user")."</div>");
 if (PNApplication::$instance->user_management->has_right("consult_user_rights"))
 	$list->add_item_action("/static/user_management/access_list.png",get_locale("Access Rights"),"user_rights?domain=%Users.domain%&username=%Users.username%");
 $list->build($this);
+
+$this->add_javascript("/static/common/js/component/popup_window.js");
+$this->add_stylesheet("/static/common/js/component/popup_window.css");
+if (PNApplication::$instance->user_management->has_right("assign_role")) {
 ?>
+<div style='visibility:hidden;position:absolute;top:-10000px;' id='assign_roles_popup'>
+<?php
+echo " <span id='assign_unassign'></span> ";
+echo " <span id='nb_assign'></span> ";
+locale("users");
+echo ":<br/>";
+echo "<form name='assign_roles_form' onsubmit='return false'>";
+$roles = DataModel::get()->getTable("Role")->select()->order_by('Role','name')->execute();
+foreach ($roles as $role) {
+	echo "<input type='checkbox' name='role_".$role['id']."'/> ".$role['name']."<br/>";
+}
+echo "</form>";
+?>
+</div>
+<script type='text/javascript'>
+function um_users_assign_roles() {
+	var sel = datalist_grid.getSelection();
+	var data = {};
+	for (var i = 0; i < sel.length; ++i)
+		data["user"+i] = datalist_data_keys[sel[i]];
+	var content = document.getElementById('assign_roles_popup');
+	document.getElementById('nb_assign').innerHTML = sel.length;
+	document.getElementById('assign_unassign').innerHTML = "<?php locale("Assign the following roles to the");?>";
+	var popup = new popup_window("<?php locale("Assign roles")?>","/static/user_management/role.png",content);
+	popup.keep_content_on_close = true;
+	popup.addOkCancelButtons(function() {
+		var form = document.forms['assign_roles_form'];
+		var j = 0;
+		for (var i = 0; i < form.elements.length; ++i)
+			if (form.elements[i].checked)
+				data["role"+(j++)] = form.elements[i].name.substring(5);
+		pn.ajax_service_json("/dynamic/user_management/service/assign_roles",data,function(result) {
+			popup.close();
+			if (result)
+				location.reload();
+		},true);
+	});
+	popup.show();
+}
+function um_users_unassign_roles() {
+	var sel = datalist_grid.getSelection();
+	var data = {};
+	for (var i = 0; i < sel.length; ++i)
+		data["user"+i] = datalist_data_keys[sel[i]];
+	var content = document.getElementById('assign_roles_popup');
+	document.getElementById('nb_assign').innerHTML = sel.length;
+	document.getElementById('assign_unassign').innerHTML = "<?php locale("Unassign the following roles to the");?>";
+	var popup = new popup_window("<?php locale("Unassign roles")?>","/static/user_management/role.png",content);
+	popup.keep_content_on_close = true;
+	popup.addOkCancelButtons(function() {
+		var form = document.forms['assign_roles_form'];
+		var j = 0;
+		for (var i = 0; i < form.elements.length; ++i)
+			if (form.elements[i].checked)
+				data["role"+(j++)] = form.elements[i].name.substring(5);
+		pn.ajax_service_json("/dynamic/user_management/service/unassign_roles",data,function(result) {
+			popup.close();
+			if (result)
+				location.reload();
+		},true);
+	});
+	popup.show();
+}
+</script>
+<?php }?>
